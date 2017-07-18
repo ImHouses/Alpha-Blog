c155be4448afa09683f87f8025d9252b8925398e
Juan Casas <imhouses@ciencias.unam.mx>
2017-07-18 17:49:15 -0500

    Implemented new and edit actions to users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}, you have signed up successfully"
+      redirect_to(articles_path)
+    else
+      flash[:error] = user_errors(@user)
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update
+      flash[:success] = "Your account was updated successfully"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def user_errors(user)
+      user.errors.full_messages
+    end
+end
